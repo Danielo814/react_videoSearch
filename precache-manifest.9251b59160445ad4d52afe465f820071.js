db8244bdc3a244e896ebb7c62b32e0effd5abcb8
Danielo814 <ojedadaniel92@gmail.com>
2020-02-27 20:07:27 -0800

    Updates

---
+++ precache-manifest.9251b59160445ad4d52afe465f820071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ec6c2f0ef5f6193e891d1716361655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee3a6d4db645cb08675",
+    "url": "/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "ff1e8fd24fc739ff85a8",
+    "url": "/static/js/2.85523917.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.85523917.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ee3a6d4db645cb08675",
+    "url": "/static/js/main.22b8944d.chunk.js"
+  },
+  {
+    "revision": "b349636d7ec7319b13f4",
+    "url": "/static/js/runtime-main.5132ebb7.js"
+  }
+]);
