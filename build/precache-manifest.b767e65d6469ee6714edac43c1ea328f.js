e886605817a8d9c4ab945cfbf8d56f9869998397
Danielo814 <ojedadaniel92@gmail.com>
2020-02-27 20:01:54 -0800

    add remaining

---
+++ build/precache-manifest.b767e65d6469ee6714edac43c1ea328f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f7bf52767f3c027396ba5093585719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8dd7c8b0fca2ab05100",
+    "url": "/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "ff1e8fd24fc739ff85a8",
+    "url": "/static/js/2.85523917.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.85523917.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8dd7c8b0fca2ab05100",
+    "url": "/static/js/main.00dbecda.chunk.js"
+  },
+  {
+    "revision": "b349636d7ec7319b13f4",
+    "url": "/static/js/runtime-main.5132ebb7.js"
+  }
+]);
